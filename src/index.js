3977a248509e992b03f2de8e8c406a5be2a244fc
Jose De Las Salas <jld03e@gmail.com>
2019-08-02 14:22:39 -0400

    added comment cards to page, as per lecture, and included a approvereject component with reusable features and structure.

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Sam"
+          timeAgo="Today at 4:45pm"
+          textPost="pinga frita."
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Alex"
+          timeAgo="Today at 2:00am"
+          textPost="it drives me crazy."
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author="Jane"
+          timeAgo="Yesterday at 5:00pm"
+          textPost="who's on first."
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
